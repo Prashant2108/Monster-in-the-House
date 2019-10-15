90b978163d417ea79e2232cd3395f3b4b40bfcb5
Prashant Agnihotri <49424915+Prashant2108@users.noreply.github.com>
2019-10-14 20:47:15 -0700

    Updates

---
+++ precache-manifest.7dc165a5c93fbf1f7517fbd9883b791b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0296aa96730e4a502dc31317071e0f3c",
+    "url": "/Monster%20in%20the%20House/index.html"
+  },
+  {
+    "revision": "1a7cd334b86ff4b28888",
+    "url": "/Monster%20in%20the%20House/static/css/main.7bd5be70.chunk.css"
+  },
+  {
+    "revision": "0eade83a1fb4d6639cdf",
+    "url": "/Monster%20in%20the%20House/static/js/2.6c2f71aa.chunk.js"
+  },
+  {
+    "revision": "1a7cd334b86ff4b28888",
+    "url": "/Monster%20in%20the%20House/static/js/main.221bd6a2.chunk.js"
+  },
+  {
+    "revision": "c4fc0b19fec8cf6c42a1",
+    "url": "/Monster%20in%20the%20House/static/js/runtime-main.8fa18a5a.js"
+  }
+]);
